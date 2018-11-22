78739f852149d6dababe456a682f01d20e824a7b
LucasNFalcao <lucasfalca23@hotmail.com>
2018-11-22 00:39:02 -0200

    Alterando os métodos da classe Mesa

--- codigo/OsVingadores/src/osvingadores/Mesa.java
+++ codigo/OsVingadores/src/osvingadores/Mesa.java
@@ -1,43 +1,69 @@
 package OsVingadores;
 
+import java.util.ArrayList;
+
 public class Mesa implements Jogada {
 	protected Monte _monteCompra;
 	protected Monte _monteVilao;
 	protected Monte _monteDescarte;
 	protected Monte _monteVilaosAtivos;
 	protected boolean _partidaEmAndamento;
-	protected Jogador _jogadores;
+	protected ArrayList<Jogador> _jogadores;
 	protected boolean _comJogadorVencedor;
 	public Jogador _unnamed_Jogador_;
 	public ControladorJogo _unnamed_ControladorJogo_;
 	public Monte _unnamed_Monte_;
+        
+        
+        public Mesa(){
+            _monteCompra = new Monte();
+            _monteVilao = new Monte();
+            _monteDescarte = new Monte();
+            _monteVilaosAtivos = new Monte();
+            _partidaEmAndamento = true;
+            _jogadores = new ArrayList();
+            _comJogadorVencedor = false;
+                    
+        }
 
 	public boolean isPartidaEmAndamento() {
-		return this._partidaEmAndamento;
+            return this._partidaEmAndamento;
 	}
 
-	public Jogador getColecaoJogadores() {
-		throw new UnsupportedOperationException();
+	public ArrayList<Jogador> getColecaoJogadores() {
+            return this._jogadores;
+            //throw new UnsupportedOperationException();
 	}
 
 	public void removerVilao(Personagem aVilao) {
-		throw new UnsupportedOperationException();
+            this._monteVilaosAtivos.removerCarta(aVilao);
+            //throw new UnsupportedOperationException();
 	}
 
 	public boolean isJogadorVencedor() {
-		throw new UnsupportedOperationException();
+            return _comJogadorVencedor;
+            //throw new UnsupportedOperationException();
 	}
 
 	public Jogador temJogadorVencedor() {
+            int tamanho = this._monteVilaosAtivos.tamanhoMonte();
+            if(tamanho == 0){
+                this.setComJogadorVencedor(_comJogadorVencedor);
+                _unnamed_Jogador_ = encontrarJogadorVencedor();
+            }
 		throw new UnsupportedOperationException();
 	}
 
-	public void setComJogadorVencedor(boolean aTemjogadorVencedor) {
+	public void setComJogadorVencedor(boolean aTemjogadorVencedor){
 		this._comJogadorVencedor = aTemjogadorVencedor;
 	}
 
 	public Jogador encontrarJogadorVencedor() {
-		throw new UnsupportedOperationException();
+            Jogador jogadorRef = new Jogador();
+            for(int i = 0; i < 2; i++){
+                jogadorRef = _jogadores.get(i);
+            }
+            throw new UnsupportedOperationException();
 	}
 
 	public TipoArtefato verificarQualArtefato(Artefato aCartaArtefato) {
@@ -45,7 +71,9 @@ public class Mesa implements Jogada {
 	}
 
 	public Carta comprarCartaDoMonteCompra() {
-		throw new UnsupportedOperationException();
+            Carta novaCarta = this._monteCompra.comprarCarta();
+            return novaCarta;
+            //throw new UnsupportedOperationException();
 	}
 
 	public void trocarMaosDosJogadores() {
