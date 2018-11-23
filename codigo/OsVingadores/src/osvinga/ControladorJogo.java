994b44ad257bcdcae3b16c74c476056213891a25
Joao Vitor Venancio <joaovitorvenancio99@gmail.com>
2018-11-23 14:39:31 -0200

    Renomeado atributos + metodo iniciarPartida

--- codigo/OsVingadores/src/osvinga/ControladorJogo.java
+++ codigo/OsVingadores/src/osvinga/ControladorJogo.java
@@ -1,94 +1,162 @@
 package osvinga;
 
+import java.util.ArrayList;
+
 public class ControladorJogo {
-	protected Mesa _mesa;
-	protected String _nomeJogador;
-	protected boolean _conectado;
-	public AtorJogador _atorJogador_;
-	public Mesa _mesa_;
-	public AtorNetGames _atorNetGames_;
 
-	public int conectar() {
-		throw new UnsupportedOperationException();
-	}
+    protected Mesa mesa;
+    protected String nomeJogador;
+    protected boolean conectado;
+    protected AtorJogador atorJogador;
+    protected AtorNetGames atorNetGames;
 
-	public boolean isConectado() {
-		return this._conectado;
-	}
+    public int conectar() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setNomeJogador(String aNomeJogador) {
-		this._nomeJogador = aNomeJogador;
-	}
+    public boolean isConectado() {
+        return this.conectado;
+    }
 
-	public void iniciarPartida() {
-		throw new UnsupportedOperationException();
-	}
+    public void setNomeJogador(String aNomeJogador) {
+        this.nomeJogador = aNomeJogador;
+    }
 
-	public Mesa getMesa() {
-		return this._mesa;
-	}
+    public void iniciarPartida() {
+        boolean conectado = this.isConectado();
+        boolean emAndamento = false;
+        if (conectado) {
+            Mesa mesa = this.getMesa();
+            
+            if (mesa != null) {
+                emAndamento = mesa.isPartidaEmAndamento();
+            }
+            
+            if (emAndamento) {
+                this.getAtorJogador().notificarPartidaEmAndamento();
+                return;
+            } else {
+                boolean semConexao = this.getAtorNetGames().iniciarPartida();
+                
+                if (semConexao) {
+                    this.getAtorJogador().notificarNaoConectado();
+                    return;
+                } else {
+                    this.getAtorJogador().notificarSucessoIniciarPartida();
+                    this.receberSolicitacaoDeInico();
+                    return;
+                }
+            }
+        }
+        
+        this.getAtorJogador().notificarNaoConectado();
+        return;
+    }
 
-	public boolean getPartidaEmAndamento() {
-		throw new UnsupportedOperationException();
-	}
+    public Mesa getMesa() {
+        return this.mesa;
+    }
 
-	public boolean desconectar() {
-		throw new UnsupportedOperationException();
-	}
+    public boolean getPartidaEmAndamento() {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean passarTurno() {
-		throw new UnsupportedOperationException();
-	}
+    public boolean desconectar() {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean verificarJogadorDoTurno() {
-		throw new UnsupportedOperationException();
-	}
+    public boolean passarTurno() {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean capturarVilao(Monte aCartasCapturar, Carta aVilao) {
-		throw new UnsupportedOperationException();
-	}
+    public boolean verificarJogadorDoTurno() {
+        throw new UnsupportedOperationException();
+    }
 
-	public int calcularSomatorioDePoder(Monte aCartasCapturar) {
-		throw new UnsupportedOperationException();
-	}
+    public boolean capturarVilao(Monte cartasCapturar, Carta vilao) {
+        boolean resultado = false;
+        int somatorioDePontos = this.calcularSomatorioDePoder(cartasCapturar);
+        int poderVilao = vilao.getPoder();
+        
+        if(somatorioDePontos >= poderVilao) {
+            Jogador jogador = this.recuperarInstanciaJogador();
+            
+            jogador.adicionarVilao(vilao);
+            jogador.retirarConjuntoDaMao(cartasCapturar);
+            
+            this.getMesa().removerVilao(vilao);
+            
+            resultado = true;
+            
+        }
+        
+        this.verificarEstadoDoJogo();
+        
+        return resultado;
+        
+    }
 
-	public Jogador recuperarInstanciaJogador() {
-		throw new UnsupportedOperationException();
-	}
+    public int calcularSomatorioDePoder(Monte aCartasCapturar) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void receberEstadoDaMesa(Mesa aMesa) {
-		throw new UnsupportedOperationException();
-	}
+    public Jogador recuperarInstanciaJogador() {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean verificarEstadoDoJogo() {
-		throw new UnsupportedOperationException();
-	}
+    public void receberEstadoDaMesa(Mesa aMesa) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setMesa(Mesa aMesa) {
-		this._mesa = aMesa;
-	}
+    public boolean verificarEstadoDoJogo() {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean verificarEstadoJogo() {
-		throw new UnsupportedOperationException();
-	}
+    public void setMesa(Mesa aMesa) {
+        this.mesa = aMesa;
+    }
 
-	public boolean cartaLocalizadaEmMaos() {
-		throw new UnsupportedOperationException();
-	}
+    public boolean verificarEstadoJogo() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void usarJoia(Artefato aCartaJoia) {
-		throw new UnsupportedOperationException();
-	}
+    public boolean cartaLocalizadaEmMaos() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void verificarQualArtefato(Artefato aCarta) {
-		throw new UnsupportedOperationException();
-	}
+    public void usarJoia(Artefato aCartaJoia) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void atualizarEstadoJogo() {
-		throw new UnsupportedOperationException();
-	}
+    public void verificarQualArtefato(Artefato aCarta) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void enviarJogada(Mesa aMesa) {
-		throw new UnsupportedOperationException();
-	}
+    public void atualizarEstadoJogo() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void enviarJogada(Mesa aMesa) {
+        throw new UnsupportedOperationException();
+    }
+
+    public AtorJogador getAtorJogador() {
+        return atorJogador;
+    }
+
+    public void setAtorJogador(AtorJogador atorJogador) {
+        this.atorJogador = atorJogador;
+    }
+
+    public AtorNetGames getAtorNetGames() {
+        return atorNetGames;
+    }
+
+    public void setAtorNetGames(AtorNetGames atorNetGames) {
+        this.atorNetGames = atorNetGames;
+    }
+
+    private void receberSolicitacaoDeInico() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
