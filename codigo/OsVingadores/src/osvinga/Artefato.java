e95fb19eabe87cb4b38ef9ca599fc13fd779f4c9
joaovenancio <joaovitorvenancio99@outlook.com>
2018-11-25 14:28:25 -0200

    Corrigido a identacao

--- codigo/OsVingadores/src/osvinga/Artefato.java
+++ codigo/OsVingadores/src/osvinga/Artefato.java
@@ -1,22 +1,23 @@
 package osvinga;
 
 public class Artefato extends Carta {
-    //Atributos:
-	protected TipoArtefato tipo;
 
-        //Construtor:
-    public Artefato(String nome, int poder,TipoArtefato tipo) {
-        super(nome,poder);
+    //Atributos:
+    protected TipoArtefato tipo;
+
+    //Construtor:
+    public Artefato(String nome, int poder, TipoArtefato tipo) {
+        super(nome, poder);
         this.tipo = tipo;
-    }   
+    }
 
     //Metodos:
-	public TipoArtefato getTipo() {
-            return this.tipo;
-	}
+    public TipoArtefato getTipo() {
+        return this.tipo;
+    }
 
     public void setTipo(TipoArtefato tipo) {
         this.tipo = tipo;
     }
-        
+
 }
