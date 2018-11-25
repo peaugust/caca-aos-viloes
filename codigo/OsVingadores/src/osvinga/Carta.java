3c01327d12895ed25791f64f3c6f9bf36c696254
joaovenancio <joaovitorvenancio99@outlook.com>
2018-11-25 14:27:29 -0200

    Corrgido a idenetacao

--- codigo/OsVingadores/src/osvinga/Carta.java
+++ codigo/OsVingadores/src/osvinga/Carta.java
@@ -1,23 +1,22 @@
 package osvinga;
 
 public class Carta {
-	protected String nome;
-	protected int poder;
+
+    protected String nome;
+    protected int poder;
 
     public Carta(String nome, int poder) {
         this.nome = nome;
         this.poder = poder;
     }
-        
-        
 
-	public int getPoder() {
-		return this.poder;
-	}
+    public int getPoder() {
+        return this.poder;
+    }
 
-	public String getNome() {
-		return this.nome;
-	}
+    public String getNome() {
+        return this.nome;
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
@@ -26,6 +25,5 @@ public class Carta {
     public void setPoder(int poder) {
         this.poder = poder;
     }
-        
-        
+
 }
