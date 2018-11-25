403941b9c2e585e89a283cc9dd24b1874fd08e56
joaovenancio <joaovitorvenancio99@outlook.com>
2018-11-25 14:23:14 -0200

    Adicionado o metodo de criar o monte de viloes e herois

--- codigo/OsVingadores/src/osvinga/Mesa.java
+++ codigo/OsVingadores/src/osvinga/Mesa.java
@@ -2,6 +2,7 @@ package osvinga;
 
 import br.ufsc.inf.leobr.cliente.Jogada;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Mesa implements Jogada {
 
@@ -58,13 +59,13 @@ public class Mesa implements Jogada {
     public Jogador encontrarJogadorVencedor() {
         Jogador jogador1 = this.jogadores.get(0);
         Jogador jogador2 = this.jogadores.get(1);
-        
+
         if (jogador2.temPontuacaoMaior(jogador1.getPontuacao())) {
             return jogador2;
         } else {
             return jogador1;
         }
-        
+
     }
 
     public TipoArtefato verificarQualArtefato(Artefato cartaArtefato) {
@@ -79,13 +80,13 @@ public class Mesa implements Jogada {
     public void trocarMaosDosJogadores() {
         Jogador jogador1 = this.jogadores.get(0);
         Jogador jogador2 = this.jogadores.get(1);
-        
+
         Monte maoJogador1 = jogador1.getMao();
         Monte maoJogador2 = jogador2.getMao();
-        
+
         jogador1.setMaoJogador(maoJogador2);
         jogador2.setMaoJogador(maoJogador1);
-        
+
         return;
     }
 
@@ -128,6 +129,61 @@ public class Mesa implements Jogada {
     public void setJogadores(ArrayList<Jogador> jogadores) {
         this.jogadores = jogadores;
     }
-    
-    
+
+    public void cirarMonteDeCompra() {
+        Monte novoMonteCompra = new Monte();
+
+        novoMonteCompra.adicionarCarta(new Personagem("Groot", 1, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Bucky", 1, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Shuri", 1, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Wong", 1, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Matins", 1, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Falcão", 1, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Homem-Aranha", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Thor", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Okoye", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Feiticeira Escarlate", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Rocky", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Pantera Negra", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Hulk", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Gamora", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Doutor Estranho", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Drax", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Máquina de Combate", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Visão", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Senhor das Estrelas", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Viúva Negra", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Hulkbuster", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Nebulosa", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Capitão América", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Homem de Ferro", 2, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Pantera Negra e Shuri", 3, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Doutro Estranho e Wong", 3, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Groot e Rocky", 3, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Homem de Ferro e Homem-Aranha", 4, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Senhor das Estrelas e Gamora", 4, TipoPersonagem.HEROI));
+        novoMonteCompra.adicionarCarta(new Personagem("Thor e Hulk", 4, TipoPersonagem.HEROI));
+
+        Collections.shuffle(novoMonteCompra.getCartas());
+        this.setMonteCompra(novoMonteCompra);
+    }
+
+    public void criarMonteVilao() {
+        Monte novoMonteVilao = new Monte();
+        
+        novoMonteVilao.adicionarCarta(new Personagem("Batedor da Ordem Negra",5,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Batedor da Ordem Negra",5,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Batedor da Ordem Negra",5,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Batedor da Ordem Negra",5,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Batedor da Ordem Negra",5,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Abate Obsidiano",8,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Fauce de Ébano",8,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Corvus Glaive",10,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Próxima Meia-Noite",10,TipoPersonagem.VILAO));
+        novoMonteVilao.adicionarCarta(new Personagem("Thanos",15,TipoPersonagem.VILAO));
+        
+        Collections.shuffle(novoMonteVilao.getCartas());
+        this.setMonteVilao(novoMonteVilao);
+    }
+
 }
